22d582ab67c48c5a254f036050957efbcbc075c3
Martin Alexanders Lopez Mamani <lopezalexanders@gmail.com>
2025-11-09 02:06:50 -0400

    Delete backend directory

--- backend/src/middlewares/auth.ts
+++
@@ -1,9 +0,0 @@
-import { IncomingMessage } from 'http';
-
-export interface AuthenticatedRequest extends IncomingMessage {
-  user?: { id: string };
-}
-
-export function checkAuth(req: AuthenticatedRequest): void {
-  req.user = { id: 'demo-user' };
-}
